4b3638d6e17db39d3a4d85ac56028a5a72dc42f8
yoyoyoyo111 <videostormer2498@gmail.com>
2020-04-28 17:58:00 +0100

    a whole lot

--- newting.py
+++ newting.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import datetime
+
 
 app = Flask(__name__)
+app.debug = True
+
 
 @app.route('/')
 def index():
-	return "hello"
+    now = datetime.datetime.now()
+    new_year= now.month == 1 and now.day ==1
+    names=["evelynn", "kayn", "hecarim", "shyvana", "graves"]
+    return render_template("hello2.html", title1=new_year, names=names)
+
+
+@app.route('/more', methods=["GET","POST"])
+def more():
+	name=request.form.get("name")
+	return render_template("index.html", name=name)
+
